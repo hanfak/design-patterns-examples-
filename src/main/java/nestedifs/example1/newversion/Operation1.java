4144a8705defdbd10bd41f1691788e4d963c724e
Hanif Fakira <fakira.work@gmail.com>
2019-01-13 13:58:41 +0000

    refactoring nested ifs

---
+++ src/main/java/nestedifs/example1/newversion/Operation1.java
@@ -0,0 +1,30 @@
+package nestedifs.example1.newversion;
+
+public class Operation1 {
+  private Checker checker;
+
+  public Operation1(Checker checker) {
+    this.checker = checker;
+  }
+
+  public String multipleOperations() {
+
+    if (!checker.operation1()) {
+      return "Failure 1";
+    }
+
+    if (!checker.operation2()) {
+       return "Failure 2";
+    }
+
+    if (!checker.operation3()) {
+       return "Failure 3";
+    }
+
+    if (!checker.operation4()) {
+       return "Failure 4";
+    }
+
+    return "Passed";
+  }
+}
---
+++ src/main/java/nestedifs/example1/old/Checker.java
@@ -0,0 +1,11 @@
+package nestedifs.example1.old;
+
+public interface Checker {
+  boolean operation1();
+
+  boolean operation2();
+
+  boolean operation3();
+
+  boolean operation4();
+}
