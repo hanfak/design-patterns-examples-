ccd335a17bc555ba910d0148363e74fb19703aa7
Hanif Fakira <fakira.work@gmail.com>
2020-02-15 16:01:09 +0000

    libraries

---
+++ src/main/java/libraries/datetime/LocalDateTimeExample.java
@@ -0,0 +1,34 @@
+package libraries.datetime;
+
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.TemporalQueries;
+
+public class LocalDateTimeExample {
+    public static void main(String[] args) {
+        LocalDateTime timeInTheFuture = LocalDateTime.of(2018, 12, 30, 12, 23, 34);
+        System.out.println("timeInTheFuture = " + timeInTheFuture);
+
+        LocalDateTime dateTimeNow = LocalDateTime.now();
+        long timeInDaysBetweenNowAndSomeDayInTheFuture = dateTimeNow.until(timeInTheFuture, ChronoUnit.DAYS);
+        System.out.println("timeInDaysBetweenNowAndSomeDayInTheFuture = " + timeInDaysBetweenNowAndSomeDayInTheFuture);
+
+        LocalTime gettingLocalTimeFromDateTime = dateTimeNow.query(TemporalQueries.localTime());
+        System.out.println("gettingLocalTimeFromDateTime = " + gettingLocalTimeFromDateTime);
+        LocalDate gettingLocalDateFromDateTime = dateTimeNow.query(TemporalQueries.localDate());
+        System.out.println("gettingLocalDateFromDateTime = " + gettingLocalDateFromDateTime);
+        LocalDate convertToLocalDate = dateTimeNow.toLocalDate();
+        System.out.println("convertToLocalDate = " + convertToLocalDate);
+
+        LocalDateTime dateTimeNowOnZone = LocalDateTime.now(ZoneId.of("ECT", ZoneId.SHORT_IDS));
+        System.out.println("dateTimeNowOnZone = " + dateTimeNowOnZone);
+
+        LocalDateTime plusNumberOfDaysUsingDuration = timeInTheFuture.plus(Duration.ofDays(25));
+        System.out.println("plusNumberOfDaysUsingDuration = " + plusNumberOfDaysUsingDuration);
+
+        LocalDateTime dateChangedToFirstDayOfMonth = timeInTheFuture.with(TemporalAdjusters.firstDayOfMonth());
+        System.out.println("dateChangedToFirstDayOfMonth = " + dateChangedToFirstDayOfMonth);
+
+    }
+}
