c9160de719dca43b6a48f079525278309901c05f
hanfak <fakira.work@gmail.com>
2022-04-28 12:03:23 +0100

    Extract filesapi as module

--- filesapi/src/test/java/filesapi/path/PathManualTest.java
+++ filesapi/src/test/java/filesapi/path/PathManualTest.java
@@ -8,9 +8,7 @@ import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class PathManualTest {
 
